1d46fd59392519991578d5ac30e5f09256d3ce25
Yunen He <heyunen@hotmail.com>
2017-12-23 05:25:29 +0800

    Update substitution.hs

--- substitution.hs
+++ substitution.hs
@@ -23,7 +23,8 @@ apply s (TFun t1 t2) = TFun (apply s t1) (apply s t2)
 apply s t = t
 
 composeSubst :: Subst -> Subst -> Subst
-composeSubst s1 s2 = Map.union (Map.map (apply s1) s2) s1
+composeSubst s1 s2 = Map.union (Map.map (apply s2) s1) s2
+-- composeSubst s1 s2 = Map.union (Map.map (apply s1) s2) s1 (wrong !!!)
 
 -- tests
 test1 = 
